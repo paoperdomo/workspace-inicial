3424ee11623bc17765f9761d7c6aa98597578e96
paoperdomo <paoperdomo.uy@gmail.com>
2022-08-21 14:04:30 -0300

    Agregue Inicio de sesión con cuenta de google

--- js/login.js
+++ js/login.js
@@ -18,3 +18,39 @@ document.addEventListener('DOMContentLoaded', () => {
         userValidation();
     });
 });
+
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
+
+
+// Google Sign In
+function handleCredentialResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+    let userObject = parseJwt (response.credential);
+    console.log(userObject);
+    if (userObject =! null){
+        sessionStorage.setItem("user", "true")
+        window.location.href= 'index.html';
+    }
+}
+
+window.onload = function () {
+    google.accounts.id.initialize({
+        client_id: "322835120510-8dohgtcf2e9a3pn9pankgv5jt4gtso3r.apps.googleusercontent.com",
+        callback: handleCredentialResponse
+    });
+
+    google.accounts.id.renderButton(
+        document.getElementById("buttonDiv"),
+        { theme: "outline", size: "large" }  
+    );
+
+    google.accounts.id.prompt(); 
+}
